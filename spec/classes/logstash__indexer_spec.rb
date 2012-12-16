b7f10dc732706b0139eb068e359d862811bb5774
Gareth Rushgrove <gareth@morethanseven.net>
2012-12-16 22:37:22 +0000

    test the passing version behavious

--- spec/classes/logstash__indexer_spec.rb
+++ spec/classes/logstash__indexer_spec.rb
@@ -3,4 +3,10 @@ require 'spec_helper'
 describe 'logstash::indexer', :type => :class do
   let(:facts) { {:osfamily => 'Debian', :lsbdistcodename => 'precise'} }
   it { should include_class('logstash::params') }
+  it { should contain_package('java') }
+
+  context 'with a version' do
+    let(:params) { {'version' => '1.0.0'} }
+    it { should contain_exec('download logstash jar').with_creates("/usr/local/bin/logstash-1.0.0-monolithic.jar")}
+  end
 end
--- spec/classes/logstash__shipper_spec.rb
+++ spec/classes/logstash__shipper_spec.rb
@@ -4,4 +4,9 @@ describe 'logstash::shipper', :type => :class do
   let(:facts) { {:osfamily => 'Debian', :lsbdistcodename => 'precise'} }
   it { should include_class('logstash::params') }
   it { should contain_package('java') }
+
+  context 'with a version' do
+    let(:params) { {'version' => '1.0.0'} }
+    it { should contain_exec('download logstash jar').with_creates("/usr/local/bin/logstash-1.0.0-monolithic.jar")}
+  end
 end
